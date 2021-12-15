16481aa1ce550d273ce623ead82e82d1b81f6cce
entr0pia <30486766+entr0pia@users.noreply.github.com>
2021-12-15 17:04:39 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# TrichromeLibrary Squoosh Magisk Module
+
+Remove the outdated TrichromeLibrary. Reference [TrichromeLibrary Squoosh](https://github.com/entr0pia/TrichromeLibrary-Squoosh)
---
+++ service.sh
@@ -0,0 +1,14 @@
+#!/system/bin/sh
+# Do NOT assume where your module will be located.
+# ALWAYS use $MODDIR if you need to know where this script
+# and module is placed.
+# This will make sure your module will still work
+# if Magisk change its mount point in the future
+MODDIR=${0%/*}
+
+# This script will be executed in late_start service mode
+
+trib_vers=$(find /data/app -name  'com.google.android.trichromelibrary*' | grep -oE '[0-9]{9}' | sort -n | sed '$d')
+for version in $trib_vers; do
+    pm uninstall com.google.android.trichromelibrary_$version
+done
